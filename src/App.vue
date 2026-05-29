409db6ed428d61981137bf0c1e654cf80e1611b0
Casper <wcc723@users.noreply.github.com>
2026-05-29 18:11:04 +0800

    fix: set global html lang to zh-Hant-TW for correct SSG output

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import { useHead } from '@unhead/vue'
+useHead({ htmlAttrs: { lang: 'zh-Hant-TW' } })
 </script>
 <template>
   <DefaultLayout><RouterView /></DefaultLayout>
